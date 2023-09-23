835ca35a35fc7c8aa1a5a450c4baa573800b9233
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-23 14:27:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-terraform 
+provider "aws" {
+   region = "us-east-1"
+   profile = "default" 
+}
+
+variable "ami" {
+  description = "this is to select the ami option "
+}
+
+variable "instance_type" {
+ description = "this is for the type of instance "
+
+}
+
+module "aws-ec2" {
+    source = "./module"
+    ami = var.ami
+    instance_type = var.instance_type
+}
